8be610f435d9f766a5d114ac9f3fda104ffd7f43
Bhoomi Jain <bhoomijn4@gmail.com>
2026-06-26 10:10:10 +0530

    Update python practice.py

--- python practice.py
+++ python practice.py
@@ -1,5 +1,7 @@
-print("Hello, World!")
+# function and recursions
 
+print("Hello, World!")
+# function definition
 def avg():
     a=int(input("Enter the first number: "))
     b=int(input("Enter a number: "))
@@ -8,15 +10,22 @@ def avg():
 avg()
 
 print("Hello, World!")
-
+# function definition
 def avg():
     a=int(input("Enter the first number: "))
     b=int(input("Enter a number: "))
     avg=(a+b)/2
     print(avg)
+# function call
 avg()
 print("This is a simple program to calculate the average of two numbers.")
 print("Thank you for using this program!")
 avg()
+
+# quick quiz
+
+name=input("Enter your name: ")
+print("Hello, " + name + "! Welcome to the quiz.")
+
 
 
